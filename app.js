e24c45e785d6c66b338e9643e3f290ef1202e305
Ari Kurniawan <39238418+idearik@users.noreply.github.com>
2024-07-28 20:19:13 +0800

    Update for Deploy to Vercel

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 let userInputs = [];
